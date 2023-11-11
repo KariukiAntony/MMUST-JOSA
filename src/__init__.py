80a53159ed736ea0e6b87c1f50265bf3f89184b3
Kariuki Antony <antonygichoya9@gmail.com>
2023-11-11 14:38:42 +0300

    to be removed

--- src/__init__.py
+++ src/__init__.py
@@ -15,11 +15,6 @@ def create_app(config = config_dict["dev"]):
      db.init_app(app=app)
      migrate.init_app(app=app)
      JWTManager(app)
-     cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-    #  CORS(app, resources={r"/*": {"origins": "*",
-    #                              "methods": ["GET", "POST", "PATCH", "DELETE"],
-    #                              "supports_credentials": True}})
-    
      create_database(app=app)
 
      @app.after_request
@@ -39,13 +34,6 @@ def create_app(config = config_dict["dev"]):
      app.register_blueprint(auth)
      app.register_blueprint(blogs)
      
-     @app.route("/")
-     def index():
-         return jsonify({"Hello there":" Welcome to JOWA MMUST blogging web app" })
-        
-     def index():
-         return jsonify({"Hello there":" Welcome to JOWA MMUST blogging web app" })
-     
      return app
 
 
@@ -54,3 +42,66 @@ def create_database(app):
         with app.app_context():
             db.create_all()
             print("database created")
+
+""" A function for creating an application """
+def create_app(config = config_dict["dev"]):
+     
+     app = Flask(__name__)
+     app.config.from_object(config)
+     db.init_app(app=app)
+     migrate.init_app(app=app)
+     JWTManager(app)
+    #  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    #  CORS(app, resources={r"/*": {"origins": "*",
+    #                              "methods": ["GET", "POST", "PATCH", "DELETE"],
+    #  
+
+     required_headers = ["Content-Type", "Authorization"]
+     cors = CORS(app, resources={r"/*": {
+      "origins": "*",
+      "methods": ["GET", "POST", "PATCH", "DELETE"],
+      "supports_credentials": True,
+      "allow_headers": required_headers
+     }})
+
+
+    #  @app.before_request
+    #  def before_request():
+    #     if "Origin" in request.headers:
+    #         request.headers.add('Access-Control-Allow-Origin', '*')
+    #         request.headers.add('Access-Control-Allow-Headers', ', '.join(required_headers))
+
+     @app.after_request
+     def add_security_header(response):
+        response.headers["Content-Security-Policy"] = "default-src 'self'"
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = ", ".join(required_headers)
+        return response
+     
+     @app.get("/database/danger")
+     def drop_database_tables():
+          with app.app_context():
+            db.drop_all()
+            print("all database tables droped")
+            create_database()
+            print("database tables created again")
+            return jsonify({"success": "All database tables dropped"}) 
+
+     @app.errorhandler(404)
+     def handle_not_found(e):
+         return jsonify({"error": str(e)})
+     
+
+     @app.errorhandler(500)
+     def handle_Internal_server_error(e):
+         return jsonify({"error": str(e)})
+     
+     app.register_blueprint(auth)
+     app.register_blueprint(blogs)
+
+     def create_database():
+         with app.app_context():
+            db.create_all()
+            print("database tables created")
+     
+     return app
