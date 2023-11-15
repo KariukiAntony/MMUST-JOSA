ad989948690a1b06a3a62ab720efaf9d2f07033a
Kariuki Antony <antonygichoya9@gmail.com>
2023-11-15 15:54:14 +0300

    work on uploading an image

---
+++ src/views/uploads.py
@@ -0,0 +1,17 @@
+from decouple import config
+
+UPLOAD_DIRECTORY = config("UPLOAD_DIRECTORY")
+
+import cloudinary
+          
+cloudinary.config( 
+  cloud_name = "dqfjiip8v", 
+  api_key = "826449421191762", 
+  api_secret = "oNHvF0SzT4eTB1F6vdefIHH3gAg" 
+)
+
+def send_image_to_cloudinary(filename):
+    image_path = f"{UPLOAD_DIRECTORY}{filename}"
+    response = cloudinary.uploader.upload(image_path)
+    public_url = response['secure_url']
+    return public_url
