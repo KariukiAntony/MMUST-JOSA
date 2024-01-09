f9e12021577b508e76a05a0d98ad3c52e400f491
Kariuki Antony <antonygichoya9@gmail.com>
2024-01-09 12:46:11 +0300

    teardown the db after test

--- src/tests/test_admin.py
+++ src/tests/test_admin.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from .. import create_app, config_dict, db
 from werkzeug.security import generate_password_hash
@@ -14,31 +15,31 @@ class TestAdmin(unittest.TestCase):
       db.create_all()
       print("database tables created successfully")
 
-   # def test_admin_registration(self):
-   #    """
-   #    test if an admin can be created 
-   #    """
-   #    url = "/api/v1/auth/register"
-   #    data = {
-   #       "first_name": "Antony",
-   #       "last_name": "Kariuki",
-   #       "email": "user@gmail.com",
-   #       "password": "password",
-   #       "confirm": "password"
-   #    }
-   #    response = self.client.post(url, data=json.dumps(data), content_type="application/json")
-   #    # print(response.text)
-   #    self.assertEqual(response.status_code, 201)
+   def test_admin_registration(self):
+      """
+      test if an admin can be created 
+      """
+      url = "/api/v1/auth/register"
+      data = {
+         "first_name": "Antony",
+         "last_name": "Kariuki",
+         "email": "user@gmail.com",
+         "password": "password",
+         "confirm": "password"
+      }
+      response = self.client.post(url, data=json.dumps(data), content_type="application/json")
+      # print(response.text)
+      self.assertEqual(response.status_code, 201)
 
-   # def test_admin_login(self):
-   #    data = {
-   #       "email": "user@gmail.com",
-   #       "password": "password"
-   #    }
-   #    url = "/api/v1/auth/login"
-   #    response = self.client.post(url, json=data)
-   #    print(response.text)
-   #    self.assertEqual(response.status_code, 200)
+   def test_admin_login(self):
+      data = {
+         "email": "user@gmail.com",
+         "password": "password"
+      }
+      url = "/api/v1/auth/login"
+      response = self.client.post(url, json=data)
+      print(response.text)
+      self.assertEqual(response.status_code, 200)
       
    def test_admin_total_blogs(self):
       headers = {
